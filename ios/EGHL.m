06487ba0d9c879243970c2d9c514f2270334398e
MatthewSaw <sawsc94@gmail.com>
2020-03-24 12:15:27 +0800

    Remove unused code

--- ios/EGHL.m
+++ ios/EGHL.m
@@ -223,22 +223,6 @@
             [params setValue:paramValue forKey:paramName];
         }
     }
-
-    EGHLPayment *req = [[EGHLPayment alloc] init];
-    [req eGHLMPERequest:params
-         successBlock:^(PaymentRespPARAM *resp) {
-             NSDictionary *dict = [self objectAsDictionary:resp];
-             self.processingInProgress = NO;
-             [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK
-                                                                     messageAsDictionary:dict]
-                                   callbackId:[command callbackId]];
-         }
-         failedBlock:^(NSString *errorCode, NSString *errorData, NSError *error) {
-             self.processingInProgress = NO;
-             [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
-                                                                     messageAsString:errorData]
-                                   callbackId:[command callbackId]];
-         }];
 }
 
 
